411dab82bf158ec02790f2361b086af1792c5942
Someone <120342697+Somespi@users.noreply.github.com>
2022-12-28 14:30:56 +0400

    Create LabInfo.py

---
+++ src/LabInfo/LabInfo.py
@@ -0,0 +1,33 @@
+from types import ModuleType
+
+
+class LibInfo:
+    lib = None
+
+    def __init__(self, library):
+        """
+        :param library: Library class from import and NOT String
+        Upsetting the class to attach it with the class
+
+        Learn More In The Official site
+        """
+        if isinstance(library, ModuleType):
+            self.lib = library
+        else:
+            raise OSError(f"{library} is not a module ")
+
+    def fetch(self, mode):
+        """
+        :param mode: a string that has 3 cases.
+        :return: a string that is satisfied with the case of the mode
+
+        Learn More In The Official site
+        """
+        if mode == "docs":
+            return self.lib.__doc__
+        elif mode == "name":
+            return self.lib.__name__
+        elif mode == "location":
+            if getattr(self.lib, '__file__', None):
+                return self.lib.__file__
+            raise TypeError('{0} is a built-in module'.format(self.lib))
